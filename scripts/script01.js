11705f08de920e8f28f0e0aee54cc24f9d932b59
Rafael <rafaelramos0401@gmail.com>
2023-02-28 11:02:12 -0300

    fixing crescent order

--- scripts/script01.js
+++ scripts/script01.js
@@ -4,6 +4,10 @@ for (var i = 0; i < 10; i++) {
     vetor[i] = parseInt(prompt("Digite um valor (Total de 10)."));
 }
 console.log("Lista de 10 posições: " + vetor);
-var vetor2 = vetor.sort();
+var vetor2 = vetor;
+
+vetor2.sort((a, b) => {
+    return a > b ? 1 : -1;
+});
 
 console.log("Lista de ordem crescente: " + vetor2);
